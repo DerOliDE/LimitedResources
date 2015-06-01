13599c12c394afe59db9389583b5d9e5f8b15604
DerOli82 <oliver.nestler@alaoli.de>
2015-06-01 20:43:44 +0200

    Cleanup

--- src/main/java/de/alaoli/games/minecraft/mods/limitedresources/Log.java
+++ src/main/java/de/alaoli/games/minecraft/mods/limitedresources/Log.java
@@ -5,23 +5,33 @@ import org.apache.logging.log4j.Logger;
 
 public class Log 
 {
+	/********************************************************************************
+	 * Attributes
+	 ********************************************************************************/
+	
 	private static final Logger LOGGER = LogManager.getLogger( LimitedResources.MODID );
 	
+	/********************************************************************************
+	 * Methods
+	 ********************************************************************************/
+	
+	/**
+	 * Logs a message object with the WARN level.
+	 * 
+	 * @param String
+	 */
 	public static void warn( String msg )
 	{
 		LOGGER.warn( msg );
 	}
 	
+	/**
+	 * Logs a message object with the ERROR level.
+	 * 
+	 * @param String
+	 */
 	public static void error( String msg )
 	{
 		LOGGER.error( msg );
 	}	
-	
-	public static void debug( String msg )
-	{
-		if( Config.Debug.isEnabled )
-		{
-			LOGGER.debug( msg );
-		}
-	}
 }
