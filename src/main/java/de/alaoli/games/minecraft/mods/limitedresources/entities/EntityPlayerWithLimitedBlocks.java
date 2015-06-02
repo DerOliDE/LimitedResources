9f96214238371e75e96cccd70c084cda5aabd56a
DerOli82 <oliver.nestler@alaoli.de>
2015-06-02 18:01:21 +0200

    Added DimensionManager & Cleanup
    Fixed: https://github.com/DerOli82/LimitedResources/issues/2

--- src/main/java/de/alaoli/games/minecraft/mods/limitedresources/entities/EntityPlayerWithLimitedBlocks.java
+++ src/main/java/de/alaoli/games/minecraft/mods/limitedresources/entities/EntityPlayerWithLimitedBlocks.java
@@ -1,23 +1,20 @@
 package de.alaoli.games.minecraft.mods.limitedresources.entities;
 
-import java.text.ParseException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-import de.alaoli.games.minecraft.mods.limitedresources.Log;
 import de.alaoli.games.minecraft.mods.limitedresources.data.Coordinate;
 import de.alaoli.games.minecraft.mods.limitedresources.data.LimitedBlock;
 import de.alaoli.games.minecraft.mods.limitedresources.data.LimitedBlockAt;
 import de.alaoli.games.minecraft.mods.limitedresources.util.NBTUtil;
-import de.alaoli.games.minecraft.mods.limitedresources.util.ParserUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.nbt.NBTBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.nbt.NBTTagString;
 import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.IExtendedEntityProperties;
 
 public class EntityPlayerWithLimitedBlocks implements IExtendedEntityProperties
@@ -37,9 +34,19 @@ public class EntityPlayerWithLimitedBlocks implements IExtendedEntityProperties
 	public EntityPlayer entityPlayer;
 	
 	private Set<LimitedBlockAt> limitedBlocksAt;
+
+	/********************************************************************************
+	 * Methods - Constructor
+	 ********************************************************************************/
+		
+	public EntityPlayerWithLimitedBlocks( EntityPlayer entityPlayer )
+	{
+		this.entityPlayer = entityPlayer;
+		this.limitedBlocksAt = new HashSet<LimitedBlockAt>();
+	}	
 	
 	/********************************************************************************
-	 * Interface
+	 * Interface - IExtendedEntityProperties
 	 ********************************************************************************/
 	
 	@Override
@@ -56,96 +63,16 @@ public class EntityPlayerWithLimitedBlocks implements IExtendedEntityProperties
 	{
 		NBTTagCompound prop = (NBTTagCompound) compound.getTag( EXT_PROP_NAME );
 		this.limitedBlocksAt = NBTUtil.toLimitedBlockAtSet( (NBTTagList) prop.getTag( NBT_LIMITEDBLOCKSAT_SET ) );
+		
+		this.refreshAllCoordinates();
 	}	
 	
-	/**
 	@Override
-	public void saveNBTData( NBTTagCompound compound )
-	{
-		NBTTagList list;
-		NBTTagString string;
-		NBTTagCompound prop;
-		LimitedBlockAt limitedBlockAt;
-		Iterator<LimitedBlockAt> iter;
-		
-		list = new NBTTagList();
-		prop = new NBTTagCompound();
-		iter = this.limitedBlocksAt.iterator();
-		
-		while( iter.hasNext() )
-		{
-			limitedBlockAt = iter.next();
-			
-			if( limitedBlockAt.getCoordinates().size() > 0 )
-			{
-				list.appendTag( new NBTTagString( limitedBlockAt.toString() ));
-			}
-		}
-		prop.setTag( NBT_LIMITEDBLOCKSAT, list );
-		compound.setTag( EXT_PROP_NAME, prop );
-	}
-
-	@Override
-	public void loadNBTData( NBTTagCompound compound )
-	{
-		NBTTagList list;
-		String string;
-		NBTTagCompound prop;
-		
-		prop = (NBTTagCompound) compound.getTag( EXT_PROP_NAME );
-		
-		if( prop.hasKey( NBT_LIMITEDBLOCKSAT ) )
-		{
-			list = (NBTTagList) prop.getTag( NBT_LIMITEDBLOCKSAT );
-			
-			for( int i = 0; i < list.tagCount(); i++ )
-			{
-				try 
-				{
-					this.limitedBlocksAt.add( ParserUtil.parseStringtoLimitedBlockAt( list.getStringTagAt( i ) ) );
-				}
-				catch ( ParseException e ) 
-				{
-					Log.error( e.getMessage() );
-				}
-			}
-			prop.removeTag( NBT_LIMITEDBLOCKSAT );
-		}
-	}*/
-
-	@Override
-	public void init( Entity entity, World world ) 
-	{
-		//TODO Check if some blocks doesn't exists anymore (config changed)
-		
-	}
+	public void init( Entity entity, World world ) { /*Not used yet*/ }
 
 	/********************************************************************************
-	 * Methods
+	 * Methods - Getter / Setter
 	 ********************************************************************************/
-		
-	public EntityPlayerWithLimitedBlocks( EntityPlayer entityPlayer )
-	{
-		this.entityPlayer = entityPlayer;
-		this.limitedBlocksAt = new HashSet<LimitedBlockAt>();
-	}	
-
-	public boolean canPlaceBlock( World world, LimitedBlock block )
-	{
-		LimitedBlockAt limitedBlockAt;
-		Iterator<LimitedBlockAt> iter = this.limitedBlocksAt.iterator();
-				
-		while( iter.hasNext() )
-		{
-			limitedBlockAt = iter.next();
-			
-			if( limitedBlockAt.getLimitedBlock().equals( block ) )
-			{
-				return limitedBlockAt.canPlaceBlock( world );
-			}			
-		}
-		return true;
-	}
 	
 	public LimitedBlockAt getLimitedBlockAt( LimitedBlock block )
 	{
@@ -164,29 +91,138 @@ public class EntityPlayerWithLimitedBlocks implements IExtendedEntityProperties
 		return null;
 	}
 	
+	/********************************************************************************
+	 * Methods 
+	 ********************************************************************************/
+	
+	/**
+	 * Checks if all Coordinates for an Limited Block still exists.
+	 * All non-exist coordinates will be removed.
+	 * 
+	 * @param LimitedBlockAt
+	 */
+	public void refreshCoordinates( LimitedBlockAt block )
+	{
+		World world;
+		ItemStack itemStack;
+		Coordinate coordinate;
+		
+		Set<Coordinate> toRemove	= new HashSet<Coordinate>();
+		Iterator<Coordinate> iter	= block.getCoordinates().iterator();
+		
+		while( iter.hasNext() )
+		{
+			coordinate	= iter.next();
+			world		= DimensionManager.getWorld( coordinate.getDimId() );
+			itemStack	= new ItemStack(
+				world.getBlock( coordinate.getX(), coordinate.getY(), coordinate.getZ() ),
+				1,
+				world.getBlockMetadata( coordinate.getX(), coordinate.getY(), coordinate.getZ() )	
+			);
+			
+			//if Block or MetaId != remove Coordinate
+			if( ( block.getLimitedBlock().getItemStack().getItem().equals( itemStack.getItem() ) == false ) || 
+				( block.getLimitedBlock().getItemStack().getItemDamage() != itemStack.getItemDamage() ) )			
+			{
+				toRemove.add( coordinate );
+			}
+		}
+		block.getCoordinates().removeAll( toRemove );
+	}
+	
+	/**
+	 * Checks if all Coordinates for all Limited Block still exists.
+	 * All non-exist coordinates will be removed.
+	 */
+	public void refreshAllCoordinates()
+	{
+		Iterator<LimitedBlockAt> iter = this.limitedBlocksAt.iterator();
+		
+		while( iter.hasNext() )
+		{
+			this.refreshCoordinates( iter.next() );
+		}
+	}
+	
+	/**
+	 * Checks if the Player can place this Block
+	 * 
+	 * @param LimitedBlock
+	 * @return boolean
+	 */
+	public boolean canPlaceBlock( LimitedBlock block )
+	{
+		LimitedBlockAt limitedBlockAt;
+		Iterator<LimitedBlockAt> iter = this.limitedBlocksAt.iterator();
+		
+		while( iter.hasNext() )
+		{
+			limitedBlockAt = iter.next();
+			
+			//If Block is listed then...
+			if( limitedBlockAt.getLimitedBlock().equals( block ) )
+			{
+				//... refresh Coordinates
+				this.refreshCoordinates( limitedBlockAt );
+				
+				//... check if limit is reached
+				if( limitedBlockAt.getLimitedBlock().getLimit() > limitedBlockAt.getCoordinates().size() )
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Adds LimitedBlock / Coordinate
+	 * 
+	 * @param LimitedBlock
+	 * @param Coordinate
+	 */
 	public void addBlock( LimitedBlock block, Coordinate coordinate )
 	{
 		LimitedBlockAt limitedBlockAt = this.getLimitedBlockAt( block );
 		
-		
-		
+		//First entry
 		if( limitedBlockAt == null )
 		{
 			limitedBlockAt = new LimitedBlockAt( block );
 			this.limitedBlocksAt.add( limitedBlockAt );
 		}
 		
+		//Add Coordinate 
 		if( limitedBlockAt.getCoordinates().contains( coordinate ) == false )
 		{
 			limitedBlockAt.getCoordinates().add( coordinate );
 		}
 	}
 	
+	/********************************************************************************
+	 * Methods - Static 
+	 ********************************************************************************/
+	
+	/**
+	 * Register extendes properties to EntityPlayer
+	 * 
+	 * @param EntityPlayer
+	 */
 	public static final void register( EntityPlayer entityPlayer )
 	{
 		entityPlayer.registerExtendedProperties( EntityPlayerWithLimitedBlocks.EXT_PROP_NAME, new EntityPlayerWithLimitedBlocks( entityPlayer ) );
 	}
 
+	/**
+	 * Get Player with extended properties.
+	 * 
+	 * @param EntityPlayer
+	 * @return EntityPlayerWithLimitedBlocks|null
+	 */
 	public static final EntityPlayerWithLimitedBlocks get( EntityPlayer entityPlayer )
 	{
 		return (EntityPlayerWithLimitedBlocks) entityPlayer.getExtendedProperties( EntityPlayerWithLimitedBlocks.EXT_PROP_NAME );
