3931538361b1edaffcf5cf6a9ee8693906aa29b0
DerOli82 <oliver.nestler@alaoli.de>
2015-08-08 12:26:25 +0200

    Cleanup

--- src/main/java/de/alaoli/games/minecraft/mods/limitedresources/LimitedResources.java
+++ src/main/java/de/alaoli/games/minecraft/mods/limitedresources/LimitedResources.java
@@ -11,7 +11,6 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import de.alaoli.games.minecraft.mods.limitedresources.command.LimitedResourcesCommand;
