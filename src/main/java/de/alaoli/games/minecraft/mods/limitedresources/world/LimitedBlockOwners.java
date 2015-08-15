43740c970bf692f0559bed231e8019d081ece48f
DerOli82 <oliver.nestler@alaoli.de>
2015-08-15 08:23:50 +0200

    Cleanup

--- src/main/java/de/alaoli/games/minecraft/mods/limitedresources/world/LimitedBlockOwners.java
+++ src/main/java/de/alaoli/games/minecraft/mods/limitedresources/world/LimitedBlockOwners.java
@@ -14,7 +14,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTException;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldSavedData;
 
