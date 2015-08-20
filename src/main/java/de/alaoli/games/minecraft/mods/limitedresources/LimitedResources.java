6c9056b5a5b51893a9d175e552377ee14306fe10
DerOli82 <oliver.nestler@alaoli.de>
2015-08-20 09:05:50 +0200

    Version 1.1.0

--- src/main/java/de/alaoli/games/minecraft/mods/limitedresources/LimitedResources.java
+++ src/main/java/de/alaoli/games/minecraft/mods/limitedresources/LimitedResources.java
@@ -27,7 +27,7 @@ public class LimitedResources
 
 	public static final String MODID	= "limitedresources";
 	public static final String NAME		= "Limited Resources";
-	public static final String VERSION	= "1.0.1";
+	public static final String VERSION	= "1.1.0";
 						
 	/********************************************************************************
 	 * Attributes
